1016f691d2b9edb5e5dd730a451097edf361d5f8
Alvaro <alvaro.a@ies1xabia.org>
2019-01-23 20:51:54 +0100

    23/1/2019 - Miercoles

---
+++ Tema6/Ejercicio1.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        int num1=5;
+        int num2=5;
+        System.out.println("El resultado de la suma es: "+suma(num1, num2));
+    }
+    public static int suma(int a, int b) {
+        return a+b;
+    } 
+}
