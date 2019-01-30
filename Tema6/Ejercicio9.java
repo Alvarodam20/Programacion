f12609370b50ec23b3bdf053584af0f4e542ad4d
Alvaro <alvaro.a@ies1xabia.org>
2019-01-30 20:58:17 +0100

    30/01/2019 - Miercoles

--- Tema6/Ejercicio9.java
+++ Tema6/Ejercicio9.java
@@ -1,5 +1,15 @@
+//ACABAR EJERCICIO !!!!
 public class Ejercicio9 {
     public Ejercicio9() {
+        int num;
+        System.out.println("Introduce un numero: ");
+        num = Main.lector.nextInt();
+        Main.lector.nextLine();
 
+    }
+    public static void sumatorio() {
+        for (int i = 0; i >1;i++) {
+
+        }
     }
 }
